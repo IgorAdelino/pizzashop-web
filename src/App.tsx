69a3924307aa51908e6ce9f7ca311e02035d4e48
IgorAdelino <igoradelino@gmail.com>
2024-03-25 23:42:59 -0300

    feat: eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { Button } from './components/ui/button'
 import './index.css'
 
-export function App() {
+import { Button } from './components/ui/button'
 
+export function App() {
   return (
     <>
-    <div className="flex justify-center items-center h-screen">
-      <Button>Enviar</Button>
-    </div>
+      <div className="flex h-screen items-center justify-center">
+        <Button>Enviar</Button>
+      </div>
     </>
   )
 }
-
