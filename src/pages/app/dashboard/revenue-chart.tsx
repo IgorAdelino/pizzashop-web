2a4c6893c0c77613731c00f191222f6e9d9e8cec
IgorAdelino <igoradelino@gmail.com>
2024-03-31 16:50:22 -0300

    feat: pie graph

--- src/pages/app/dashboard/revenue-chart.tsx
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -1,4 +1,11 @@
-import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts'
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from 'recharts'
 import colors from 'tailwindcss/colors'
 
 import {
@@ -44,12 +51,14 @@ export function RevenueChart() {
               }
             />
             <XAxis dataKey="date" tickLine={false} axisLine={false} dy={16} />
+            <CartesianGrid vertical={false} className="stroke-muted" />
+
             <Line
               type="linear"
               strokeWidth={2}
               dataKey="revenue"
               stroke={colors.violet['500']}
-            ></Line>
+            />
           </LineChart>
         </ResponsiveContainer>
       </CardContent>
