ccc7f270d83968da5c8d265731e08ce508d185bb
IgorAdelino <igoradelino@gmail.com>
2024-05-19 14:18:51 -0300

    feat: pagination unit test

--- src/components/pagination.spec.tsx
+++ src/components/pagination.spec.tsx
@@ -7,6 +7,10 @@ import { Pagination } from './pagination'
 const onPageChangeCallBack = vi.fn()
 
 describe('Pagination', () => {
+  beforeEach(() => {
+    onPageChangeCallBack.mockClear()
+  })
+
   it('Should display the right amount of pages and results', () => {
     const wrapper = render(
       <Pagination
@@ -39,6 +43,69 @@ describe('Pagination', () => {
 
     await user.click(nextPageButton)
 
-    expect(onPageChangeCallBack).toHaveBeenCalled()
+    expect(onPageChangeCallBack).toHaveBeenCalledWith(1)
+  })
+
+  it('Should be able to navigate to the previous page', async () => {
+    const user = userEvent.setup()
+
+    const wrapper = render(
+      <Pagination
+        pageIndex={5}
+        totalCount={200}
+        perPage={10}
+        onPageChange={onPageChangeCallBack}
+      />,
+    )
+
+    const previousPageButton = wrapper.getByRole('button', {
+      name: 'Página anterior',
+    })
+
+    await user.click(previousPageButton)
+
+    expect(onPageChangeCallBack).toHaveBeenCalledWith(4)
+  })
+
+  it('Should be able to navigate to the first page', async () => {
+    const user = userEvent.setup()
+
+    const wrapper = render(
+      <Pagination
+        pageIndex={5}
+        totalCount={200}
+        perPage={10}
+        onPageChange={onPageChangeCallBack}
+      />,
+    )
+
+    const firstPageButton = wrapper.getByRole('button', {
+      name: 'Primeira página',
+    })
+
+    await user.click(firstPageButton)
+
+    expect(onPageChangeCallBack).toHaveBeenCalledWith(0)
+  })
+
+  it('Should be able to navigate to the last page', async () => {
+    const user = userEvent.setup()
+
+    const wrapper = render(
+      <Pagination
+        pageIndex={5}
+        totalCount={200}
+        perPage={10}
+        onPageChange={onPageChangeCallBack}
+      />,
+    )
+
+    const lastPageButton = wrapper.getByRole('button', {
+      name: 'Última página',
+    })
+
+    await user.click(lastPageButton)
+
+    expect(onPageChangeCallBack).toHaveBeenCalledWith(19)
   })
 })
